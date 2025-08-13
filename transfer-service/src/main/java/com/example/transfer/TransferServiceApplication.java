0b370171421dfe8653e6e86dc38a5b9e526d6cc4
F5564751 <F5564751@fnb.com>
2025-08-13 16:09:54 +0200

    Adding project skeleton.

---
+++ transfer-service/src/main/java/com/example/transfer/TransferServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferServiceApplication.class, args);
+    }
+}
